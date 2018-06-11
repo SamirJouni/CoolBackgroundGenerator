9382202f68ccb9dad69646df1eb515c7b8532f8d
F1r3byte <f1r3byte@yandex.com>
2018-06-11 23:49:27 +0200

    created basis for canvas

---
+++ assets/js/backgroundGenerator.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+	"use strict";
+
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+	var gradient;
+});
